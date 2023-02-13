db24e35a0b8be9ecd61ae554ef3df0d104ff9449
Casper Bröcheler <casper.jxb@gmail.com>
2023-02-13 17:07:24 +0100

    Gui beginning

--- core/src/main/java/com/mda/App.java
+++ core/src/main/java/com/mda/App.java
@@ -2,37 +2,29 @@ package com.mda;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    public void start(Stage stage) throws Exception {
+        //Group group = new Group();
+        Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+        Scene scene = new Scene(root);
+
+        //stage.setTitle("Multi-modal Digital Assistant");
+        //stage.setResizable(false);
+        //stage.setWidth(1280);
+        //stage.setHeight(720);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
