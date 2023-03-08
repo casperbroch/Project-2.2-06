02f48d55c4205189108cba9b79b7e15f8fd8f981
marchen03 <marian.chen@student.maastrichtuniversity.nl>
2023-03-08 13:19:28 +0100

    Fixed merging errors

---
+++ core/src/main/java/com/mda/Responder.java
@@ -0,0 +1,22 @@
+package com.mda;
+
+public class Responder {
+    int state = 0;
+
+    Responder() {}
+
+
+    // TODO: Make some switch statement for stemming and getting states
+
+    String getSkills(String message) {
+        // TODO: Linking to txt files to extract (question) format in first line
+        return ("Welcome to the " + message + " app! Would you like to add or search for information?");
+    }
+
+    String getActions(String message) {
+        // TODO: Linking to txt files to extract (question) format in first line
+        return ("Welcome to the " + message + " app! Would you like to add or search for information?");
+    }
+
+
+}
