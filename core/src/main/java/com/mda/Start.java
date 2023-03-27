52f036c14a71a503c57adb5fda321cee4ae954d9
lode <91753873+tomasschreuder@users.noreply.github.com>
2023-03-28 01:56:17 +0200

    b

---
+++ core/src/main/java/com/mda/Start.java
@@ -0,0 +1,10 @@
+package com.mda;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class Start {
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
+        App.main(args);
+    }
+}
