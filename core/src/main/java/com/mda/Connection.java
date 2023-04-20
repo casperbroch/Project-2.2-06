ad42cefc132c00b4f7e9d4e5d7ce2f548e1b5577
Casper Bröcheler <casper.jxb@gmail.com>
2023-04-20 14:40:33 +0200

    Revert "re organizing"
    This reverts commit c827c8c5ef6aaba5a464219c3bb05aa763c4d2b6.

---
+++ core/src/main/java/com/mda/Connection.java
@@ -0,0 +1,20 @@
+package com.mda;
+
+import javafx.scene.layout.VBox;
+
+// Obsolete
+public class Connection {
+
+    private Controller controller;
+    private VBox vbox;
+
+    public Connection() {
+        this.controller = App.getController();
+        this.vbox = controller.getvBox();
+    }
+
+    public void sendMessage(String message) {
+        controller.addBMessage(message, vbox);
+    }
+
+}
