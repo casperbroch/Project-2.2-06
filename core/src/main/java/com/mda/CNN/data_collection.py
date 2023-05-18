ffd22d0b9fce3a2a07cd974cff383d85af5eebe1
marchen03 <marian.chen@student.maastrichtuniversity.nl>
2023-05-18 18:14:50 +0200

    Added variations of data

--- core/src/main/java/com/mda/CNN/data_collection.py
+++ core/src/main/java/com/mda/CNN/data_collection.py
@@ -7,12 +7,12 @@ IMAGES_PATH = 'mda/CNN/data/images/'
 number_images = 30
 
 cap = cv2.VideoCapture(0)
-for imgnum in range(number_images):
-    print('Collecting image {}'.format(imgnum))
+for num in range(number_images):
+    print('Collecting image {}'.format(num))
     ret, frame = cap.read()
     if ret:
-        imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
-        cv2.imwrite(imgname, frame)
+        name = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
+        cv2.imwrite(name, frame)
         cv2.imshow('frame', frame)
         time.sleep(0.5)
 
@@ -23,7 +23,3 @@ for imgnum in range(number_images):
         break
 cap.release()
 cv2.destroyAllWindows()
-
-# https://www.youtube.com/watch?v=N_W4EYtsa10
-# 20:15
-
