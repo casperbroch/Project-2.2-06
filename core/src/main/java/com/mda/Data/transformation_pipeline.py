fd4990b895f709550d9663280c8dad2cf28ee6b8
marchen03 <marian.chen@student.maastrichtuniversity.nl>
2023-05-26 15:02:50 +0200

    Complete experiment data set

--- core/src/main/java/com/mda/Data/transformation_pipeline.py
+++ core/src/main/java/com/mda/Data/transformation_pipeline.py
@@ -11,11 +11,11 @@ import albumentations as alb
 # index = 'brightness'
 # augmentor = alb.Compose([alb.RandomBrightnessContrast(brightness_limit=1, always_apply=True)])
 
-index = 'gamma'
-augmentor = alb.Compose([alb.RandomGamma(gamma_limit=(50, 150), always_apply=True)])
+# index = 'contrast'
+# augmentor = alb.Compose([alb.RandomBrightnessContrast(contrast_limit=1, always_apply=True)])
 
-# index = 'rgb'
-# augmentor = alb.Compose([alb.RGBShift(r_shift_limit=50, g_shift_limit=50, b_shift_limit=50, always_apply=True)])
+index = 'rgb'
+augmentor = alb.Compose([alb.RGBShift(r_shift_limit=50, g_shift_limit=50, b_shift_limit=50, always_apply=True)])
 
 for partition in ['faces', 'rooms']:
     for image in os.listdir(os.path.join(partition)):
